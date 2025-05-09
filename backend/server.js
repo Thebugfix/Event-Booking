73c335fd11da17af810816dccc2fe92918b03f1e
Naved Shaikh <shaikhnaved0001@gmail.com>
2025-05-09 11:47:10 +0530

    added versel.json for deployment

--- backend/server.js
+++ backend/server.js
@@ -40,3 +40,6 @@ app.use((err, req, res, next) => {
 // Start server
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+// export for vecel deployment
+module.exports = app
